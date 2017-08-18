75522c0bb3c357778759dfc00a9a055d77f699a2
wesley vieira de lima <wesley.vieira.lima.1999@gmail.com>
2017-08-18 20:15:59 -0300

    Programa criado com métodos, Bugs com o Objeto por falta de conhecimento.

--- Método/ConsoleApplication1/Program.cs
+++ Método/ConsoleApplication1/Program.cs
@@ -7,18 +7,17 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        int soma(int x, int y)
+        static int soma(int x, int y)
         {
             return x + y;
         }
         static void Main(string[] args)
         {
             int n1, n2, t1;
-            Program pp = new Program();
             n1=int.Parse(Console.ReadLine());
             n2=int.Parse(Console.ReadLine());
             t1=n1+n2;
-            Console.WriteLine(n1 + " + " + n2 + " = " + pp.soma(n1, n2));
+            Console.WriteLine(n1 + " + " + n2 + " = " + soma(n1, n2));
             Console.ReadKey();
         }
     }
