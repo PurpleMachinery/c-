d31acc6265073632c455c25feb4d08fa5cbf8718
wesley vieira de lima <wesley.vieira.lima.1999@gmail.com>
2017-08-18 20:38:43 -0300

    Método gerenciais prontos

--- Método/ConsoleApplication1/Program.cs
+++ Método/ConsoleApplication1/Program.cs
@@ -7,17 +7,33 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static int soma(int x, int y)
+        static void dobro(int x)
         {
-            return x + y;
+            Console.WriteLine(x+" * 2 = "+(x*2));
+        }
+        static void digite()
+        {
+            Console.Write("Digite um numero: ");
+        }
+        static void soma(int x, int y)
+        {
+            Console.WriteLine(x + " + " + y + " = " + (x + y));
+        }
+        static void mult(int x, int y)
+        {
+            Console.WriteLine(x + " * " + y + " = " + (x * y));
         }
         static void Main(string[] args)
         {
-            int n1, n2, t1;
+            int n1, n2;
+            digite();
             n1=int.Parse(Console.ReadLine());
+            digite();
             n2=int.Parse(Console.ReadLine());
-            t1=n1+n2;
-            Console.WriteLine(n1 + " + " + n2 + " = " + soma(n1, n2));
+            soma(n1, n2);
+            mult(n1, n2);
+            dobro(n1);
+            dobro(n2);
             Console.ReadKey();
         }
     }
