e5d55297efe1bb28f692f76ae559a13118887a90
wesley vieira de lima <wesley.vieira.lima.1999@gmail.com>
2017-08-18 20:02:14 -0300

    Método trabalhado e melhorado.

--- Método/ConsoleApplication1/Program.cs
+++ Método/ConsoleApplication1/Program.cs
@@ -7,19 +7,19 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        int soma(int x, int y)
+        {
+            return x + y;
+        }
         static void Main(string[] args)
         {
-            Program pp = new Program();
             int n1, n2, t1;
+            Program pp = new Program();
             n1=int.Parse(Console.ReadLine());
             n2=int.Parse(Console.ReadLine());
             t1=n1+n2;
             Console.WriteLine(n1 + " + " + n2 + " = " + pp.soma(n1, n2));
             Console.ReadKey();
-        }
-        int soma(int x, int y)
-        {
-            return x + y;
         }
     }
 }
