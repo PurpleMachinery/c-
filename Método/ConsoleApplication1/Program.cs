6a3a6719f99c7143ef6f50d137775617d1199f03
wesley vieira de lima <wesley.vieira.lima.1999@gmail.com>
2017-08-18 19:59:07 -0300

    Primeira Método feito

---
+++ Método/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pp = new Program();
+            int n1, n2, t1;
+            n1=int.Parse(Console.ReadLine());
+            n2=int.Parse(Console.ReadLine());
+            t1=n1+n2;
+            Console.WriteLine(n1 + " + " + n2 + " = " + pp.soma(n1, n2));
+            Console.ReadKey();
+        }
+        int soma(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
