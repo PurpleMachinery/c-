6b33a196c86dc72ab63eb709ed3450679e842015
Wesley vieira de lima <wesley.vieira.lima.1999@gmail.com>
2017-08-25 22:06:51 -0300

    Exercicio extra pronto

---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex5
+{
+    class Program
+    {
+        static double hipotenusa(double altura, double basee)
+        {
+            return  Math.Sqrt((basee * basee) + (altura * altura));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite altura: ");
+            double altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite base: ");
+            double basee = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hipotenusa: "+hipotenusa(altura,basee));
+            Console.ReadKey();
+        }
+    }
+}
