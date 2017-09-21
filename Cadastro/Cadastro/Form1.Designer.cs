fb2993955813260b148341a360fb964737969f1c
Aluno <wesley.vieira.lima.1999@gmail.com>
2017-09-21 02:18:25 -0300

    Cadastro sem mascara

---
+++ Cadastro/Cadastro/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Cadastro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblSobrenome = new System.Windows.Forms.Label();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.txbSobrenome = new System.Windows.Forms.TextBox();
+            this.txbIdade = new System.Windows.Forms.TextBox();
+            this.txbSálario = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(13, 13);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblSobrenome
+            // 
+            this.lblSobrenome.AutoSize = true;
+            this.lblSobrenome.Location = new System.Drawing.Point(13, 39);
+            this.lblSobrenome.Name = "lblSobrenome";
+            this.lblSobrenome.Size = new System.Drawing.Size(61, 13);
+            this.lblSobrenome.TabIndex = 1;
+            this.lblSobrenome.Text = "Sobrenome";
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Location = new System.Drawing.Point(12, 65);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(34, 13);
+            this.lblIdade.TabIndex = 2;
+            this.lblIdade.Text = "Idade";
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Location = new System.Drawing.Point(12, 93);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(39, 13);
+            this.lblSalario.TabIndex = 3;
+            this.lblSalario.Text = "Sálario";
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(16, 142);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(256, 85);
+            this.btnVerificar.TabIndex = 4;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // txbNome
+            // 
+            this.txbNome.Location = new System.Drawing.Point(78, 10);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(194, 20);
+            this.txbNome.TabIndex = 5;
+            this.txbNome.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txbSobrenome
+            // 
+            this.txbSobrenome.Location = new System.Drawing.Point(78, 39);
+            this.txbSobrenome.Name = "txbSobrenome";
+            this.txbSobrenome.Size = new System.Drawing.Size(194, 20);
+            this.txbSobrenome.TabIndex = 6;
+            this.txbSobrenome.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txbIdade
+            // 
+            this.txbIdade.Location = new System.Drawing.Point(78, 66);
+            this.txbIdade.Name = "txbIdade";
+            this.txbIdade.Size = new System.Drawing.Size(194, 20);
+            this.txbIdade.TabIndex = 7;
+            this.txbIdade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txbSálario
+            // 
+            this.txbSálario.Location = new System.Drawing.Point(78, 93);
+            this.txbSálario.Name = "txbSálario";
+            this.txbSálario.Size = new System.Drawing.Size(194, 20);
+            this.txbSálario.TabIndex = 8;
+            this.txbSálario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 239);
+            this.Controls.Add(this.txbSálario);
+            this.Controls.Add(this.txbIdade);
+            this.Controls.Add(this.txbSobrenome);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.lblIdade);
+            this.Controls.Add(this.lblSobrenome);
+            this.Controls.Add(this.lblNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblSobrenome;
+        private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.TextBox txbSobrenome;
+        private System.Windows.Forms.TextBox txbIdade;
+        private System.Windows.Forms.TextBox txbSálario;
+
+    }
+}
+
---
+++ Cadastro/Cadastro/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cadastro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            int porc=0;
+            if (System.Convert.ToInt32(txbIdade.Text) <= 18)
+            {
+                porc = 10;
+            }
+            else if (System.Convert.ToInt32(txbIdade.Text) > 18 && System.Convert.ToInt32(txbIdade.Text) <= 25)
+            {
+                porc = 12;
+            }
+            else if (System.Convert.ToInt32(txbIdade.Text) > 26)
+            {
+                porc = 15;
+            }
+            MessageBox.Show(txbNome.Text + " " + txbSobrenome.Text + "\nAumento: "+ porc + "%" + "\nSálario Atual: " + txbSálario.Text + "\nSálario Atualizado: " + (System.Convert.ToDouble(txbSálario.Text)+((System.Convert.ToDouble(txbSálario.Text) / 100) * porc)));
+        }
+    }
+}
