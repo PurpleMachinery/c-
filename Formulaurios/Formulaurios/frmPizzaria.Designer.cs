b60fa4ae0ae272d3952264b4682fc57756cc32a9
Wesley V. Lima <wesley.vieira.lima.1999@gmail.com>
2017-10-27 16:51:14 -0200

    Segundo pronto

--- Formulaurios/Formulaurios/frmPizzaria.Designer.cs
+++ Formulaurios/Formulaurios/frmPizzaria.Designer.cs
@@ -38,15 +38,22 @@
             this.lblEndereco = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.lblExtra = new System.Windows.Forms.Label();
+            this.ckbMRecheio = new System.Windows.Forms.CheckBox();
+            this.ckbBacon = new System.Windows.Forms.CheckBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblMR = new System.Windows.Forms.Label();
+            this.lblMB = new System.Windows.Forms.Label();
+            this.mtxbResult = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
             // cbSabor
             // 
             this.cbSabor.FormattingEnabled = true;
             this.cbSabor.Items.AddRange(new object[] {
-            "Peperoni",
-            "Mussarela",
-            "Portuguesa"});
+            "Peperoni (R$ 21)",
+            "Mussarela (R$ 19)",
+            "Portuguesa (R$ 23)"});
             this.cbSabor.Location = new System.Drawing.Point(53, 43);
             this.cbSabor.Name = "cbSabor";
             this.cbSabor.Size = new System.Drawing.Size(240, 21);
@@ -105,7 +112,7 @@
             // 
             // txbEndereco
             // 
-            this.txbEndereco.Location = new System.Drawing.Point(71, 260);
+            this.txbEndereco.Location = new System.Drawing.Point(75, 204);
             this.txbEndereco.Multiline = true;
             this.txbEndereco.Name = "txbEndereco";
             this.txbEndereco.Size = new System.Drawing.Size(222, 77);
@@ -114,7 +121,7 @@
             // lblEndereco
             // 
             this.lblEndereco.AutoSize = true;
-            this.lblEndereco.Location = new System.Drawing.Point(11, 291);
+            this.lblEndereco.Location = new System.Drawing.Point(15, 236);
             this.lblEndereco.Name = "lblEndereco";
             this.lblEndereco.Size = new System.Drawing.Size(53, 13);
             this.lblEndereco.TabIndex = 7;
@@ -138,11 +145,84 @@
             this.label2.TabIndex = 9;
             this.label2.Text = "(R$ 4)";
             // 
+            // lblExtra
+            // 
+            this.lblExtra.AutoSize = true;
+            this.lblExtra.Location = new System.Drawing.Point(15, 160);
+            this.lblExtra.Name = "lblExtra";
+            this.lblExtra.Size = new System.Drawing.Size(31, 13);
+            this.lblExtra.TabIndex = 10;
+            this.lblExtra.Text = "Extra";
+            // 
+            // ckbMRecheio
+            // 
+            this.ckbMRecheio.AutoSize = true;
+            this.ckbMRecheio.Location = new System.Drawing.Point(62, 160);
+            this.ckbMRecheio.Name = "ckbMRecheio";
+            this.ckbMRecheio.Size = new System.Drawing.Size(72, 17);
+            this.ckbMRecheio.TabIndex = 11;
+            this.ckbMRecheio.Text = "+Recheio";
+            this.ckbMRecheio.UseVisualStyleBackColor = true;
+            // 
+            // ckbBacon
+            // 
+            this.ckbBacon.AutoSize = true;
+            this.ckbBacon.Location = new System.Drawing.Point(140, 160);
+            this.ckbBacon.Name = "ckbBacon";
+            this.ckbBacon.Size = new System.Drawing.Size(57, 17);
+            this.ckbBacon.TabIndex = 12;
+            this.ckbBacon.Text = "Bacon";
+            this.ckbBacon.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(88, 302);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(129, 35);
+            this.btnCalcular.TabIndex = 13;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblMR
+            // 
+            this.lblMR.AutoSize = true;
+            this.lblMR.Location = new System.Drawing.Point(85, 180);
+            this.lblMR.Name = "lblMR";
+            this.lblMR.Size = new System.Drawing.Size(36, 13);
+            this.lblMR.TabIndex = 14;
+            this.lblMR.Text = "(R$ 6)";
+            // 
+            // lblMB
+            // 
+            this.lblMB.AutoSize = true;
+            this.lblMB.Location = new System.Drawing.Point(158, 181);
+            this.lblMB.Name = "lblMB";
+            this.lblMB.Size = new System.Drawing.Size(36, 13);
+            this.lblMB.TabIndex = 15;
+            this.lblMB.Text = "(R$ 7)";
+            // 
+            // mtxbResult
+            // 
+            this.mtxbResult.Enabled = false;
+            this.mtxbResult.Location = new System.Drawing.Point(88, 380);
+            this.mtxbResult.Mask = "$ 00000";
+            this.mtxbResult.Name = "mtxbResult";
+            this.mtxbResult.Size = new System.Drawing.Size(100, 20);
+            this.mtxbResult.TabIndex = 16;
+            // 
             // frmPizzaria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(309, 455);
+            this.Controls.Add(this.mtxbResult);
+            this.Controls.Add(this.lblMB);
+            this.Controls.Add(this.lblMR);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.ckbBacon);
+            this.Controls.Add(this.ckbMRecheio);
+            this.Controls.Add(this.lblExtra);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblEndereco);
@@ -172,5 +252,12 @@
         private System.Windows.Forms.Label lblEndereco;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblExtra;
+        private System.Windows.Forms.CheckBox ckbMRecheio;
+        private System.Windows.Forms.CheckBox ckbBacon;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblMR;
+        private System.Windows.Forms.Label lblMB;
+        private System.Windows.Forms.MaskedTextBox mtxbResult;
     }
 }
--- Formulaurios/Formulaurios/frmPizzaria.cs
+++ Formulaurios/Formulaurios/frmPizzaria.cs
@@ -15,5 +15,44 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double total=0;
+            if (cbSabor.Text.ElementAt(0) == 'M' && cbSabor.Text.ElementAt(1) == 'u' && cbSabor.Text.ElementAt(2) == 's' && cbSabor.Text.ElementAt(3) == 's')
+            {
+                total += 19;
+            }
+            else if (cbSabor.Text.ElementAt(0) == 'P' && cbSabor.Text.ElementAt(1) == 'e' && cbSabor.Text.ElementAt(2) == 'p' && cbSabor.Text.ElementAt(3) == 'e') 
+            {
+                total += 21;
+            }
+            else if (cbSabor.Text.ElementAt(0) == 'P' && cbSabor.Text.ElementAt(1) == 'o' && cbSabor.Text.ElementAt(2) == 'r' && cbSabor.Text.ElementAt(3) == 't') 
+            { 
+                total +=24;
+            }
+            if (!rdbSem.Checked)
+            {
+                if (rdbCatupiri.Checked)
+                {
+                    total += 4;
+                }
+                else if (rdbCheddar.Checked) 
+                {
+                    total += 3;
+                }
+            }
+            if (ckbBacon.Checked)
+            {
+                total += 7;
+            }
+            if (ckbMRecheio.Checked)
+            {
+                total += 6;
+            }
+
+
+            mtxbResult.Text = "" + total;
+        }
     }
 }
