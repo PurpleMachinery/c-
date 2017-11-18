18f317f99039df572fec8cae38664966675476d1
wesley vieira de lima <wesley.vieira.lima.1999@gmail.com>
2017-11-17 23:31:21 -0200

    Buttons adicionados

--- Escola/Escola/Form1.Designer.cs
+++ Escola/Escola/Form1.Designer.cs
@@ -28,23 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEscola));
+            this.msMenu = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.alunosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.disciplinaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mençãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbAlunos = new System.Windows.Forms.ToolStripButton();
+            this.tsbDisciplina = new System.Windows.Forms.ToolStripButton();
+            this.tsbMencoes = new System.Windows.Forms.ToolStripButton();
+            this.msMenu.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // msMenu
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1016, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
+            this.msMenu.Location = new System.Drawing.Point(0, 0);
+            this.msMenu.Name = "msMenu";
+            this.msMenu.Size = new System.Drawing.Size(1016, 24);
+            this.msMenu.TabIndex = 1;
+            this.msMenu.Text = "menuStrip1";
             // 
             // fileToolStripMenuItem
             // 
@@ -77,19 +83,64 @@
             this.mençãoToolStripMenuItem.Text = "Menção";
             this.mençãoToolStripMenuItem.Click += new System.EventHandler(this.mençãoToolStripMenuItem_Click);
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbAlunos,
+            this.tsbDisciplina,
+            this.tsbMencoes});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1016, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbAlunos
+            // 
+            this.tsbAlunos.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbAlunos.Image = ((System.Drawing.Image)(resources.GetObject("tsbAlunos.Image")));
+            this.tsbAlunos.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbAlunos.Name = "tsbAlunos";
+            this.tsbAlunos.Size = new System.Drawing.Size(23, 22);
+            this.tsbAlunos.Text = "toolStripButton1";
+            this.tsbAlunos.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // tsbDisciplina
+            // 
+            this.tsbDisciplina.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbDisciplina.Image = ((System.Drawing.Image)(resources.GetObject("tsbDisciplina.Image")));
+            this.tsbDisciplina.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbDisciplina.Name = "tsbDisciplina";
+            this.tsbDisciplina.Size = new System.Drawing.Size(23, 22);
+            this.tsbDisciplina.Text = "toolStripButton2";
+            this.tsbDisciplina.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // tsbMencoes
+            // 
+            this.tsbMencoes.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbMencoes.Image = ((System.Drawing.Image)(resources.GetObject("tsbMencoes.Image")));
+            this.tsbMencoes.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbMencoes.Name = "tsbMencoes";
+            this.tsbMencoes.Size = new System.Drawing.Size(23, 22);
+            this.tsbMencoes.Text = "toolStripButton3";
+            this.tsbMencoes.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
             // frmEscola
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1016, 453);
-            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.msMenu);
             this.IsMdiContainer = true;
-            this.MainMenuStrip = this.menuStrip1;
+            this.MainMenuStrip = this.msMenu;
             this.Name = "frmEscola";
             this.Text = "Escola";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.msMenu.ResumeLayout(false);
+            this.msMenu.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,11 +148,15 @@
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip msMenu;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem alunosToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem disciplinaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem mençãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbAlunos;
+        private System.Windows.Forms.ToolStripButton tsbDisciplina;
+        private System.Windows.Forms.ToolStripButton tsbMencoes;
     }
 }
 
--- Escola/Escola/Form1.cs
+++ Escola/Escola/Form1.cs
@@ -36,5 +36,26 @@ namespace Escola
             a.MdiParent = this;
             a.Show();
         }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            frmAlunos a = new frmAlunos();
+            a.MdiParent = this;
+            a.Show();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            frmDiciplina a = new frmDiciplina();
+            a.MdiParent = this;
+            a.Show();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            frmMencoes a = new frmMencoes();
+            a.MdiParent = this;
+            a.Show();
+        }
     }
 }
